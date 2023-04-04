7e9609381cdd11148fd7510bf7112df1c4ba86a2
mtulijohnsn <mtulijohnsn@gmail.com>
2023-04-04 00:43:24 -0400

    complete front end , remain to deploy

---
+++ submit.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['submit'])) { // check if the form was submitted
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $
+}
+$to = 'youremail@example.com';
+$subject = 'New form submission';
+$body = "Name: $name\nEmail: $email\nMessage:\n$message";
+$headers = "From: $email\r\nReply-To: $email\r\n";
+
+mail($to, $subject, $body, $headers);
+
+echo 'Thank you for submitting the form!';
+?>
